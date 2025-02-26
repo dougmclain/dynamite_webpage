5a2a2abdc0f6c76e7245f7ad530b757d688aa0ef
Doug McLain <dougmclain@yahoo.com>
2025-02-26 14:35:14 -0800

    udate favicon settings

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import BlogCategory, BlogPost
+from .forms import BlogPostAdminForm
 
-# Register your models here.
+class BlogCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+
+class BlogPostAdmin(admin.ModelAdmin):
+    form = BlogPostAdminForm
+    list_display = ('title', 'category', 'published_date', 'is_published')
+    list_filter = ('is_published', 'category', 'published_date')
+    search_fields = ('title', 'content', 'summary')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'published_date'
+    
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'slug', 'category', 'featured_image', 'summary')
+        }),
+        ('Content', {
+            'fields': ('content',)
+        }),
+        ('Publishing', {
+            'fields': ('is_published',),
+            'classes': ('collapse',)
+        }),
+    )
+
+admin.site.register(BlogCategory, BlogCategoryAdmin)
+admin.site.register(BlogPost, BlogPostAdmin)
--- core/urls.py
+++ core/urls.py
@@ -15,13 +15,15 @@ urlpatterns = [
     # Financial Management page
     path('condo-financial-management/', views.financial_management, name='financial_management'),
     
-    # Inside urlpatterns list in core/urls.py
-    path('hoa-taxes/', views.hoa_taxes, name='hoa_taxes'),
-    
     # About page
     path('about/', views.about, name='about'),
     
-    # Blog page
+    # HOA Taxes page
+    path('hoa-taxes/', views.hoa_taxes, name='hoa_taxes'),
+    
+    # Blog pages - Note the order matters for slug patterns
+    path('blog/category/<slug:category_slug>/', views.blog_category, name='blog_category'),
+    path('blog/<slug:slug>/', views.blog_detail, name='blog_detail'),
     path('blog/', views.blog, name='blog'),
     
     # Contact page
