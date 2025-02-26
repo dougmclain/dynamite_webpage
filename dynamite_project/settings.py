9d5d233f47383da380b70b01fd188aa8d9198908
Doug McLain <dougmclain@yahoo.com>
2025-02-26 08:40:53 -0800

    update settings2

--- dynamite_project/settings.py
+++ dynamite_project/settings.py
@@ -24,7 +24,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production
 DEBUG = False
 
 # Configure allowed hosts. For production, set ALLOWED_HOSTS as an environment variable.
